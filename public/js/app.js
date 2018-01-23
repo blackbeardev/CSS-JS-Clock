587d7ae47112fd24a0d80b7956a9c96785c694ab
flyingfoxweb <erin@flyingfox.net.nz>
2018-01-23 22:26:50 +0000

    Added the js functionality to make all 3 hands move.

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hoursHand = document.querySelector(".hour-hand");
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    console.log(seconds);
+    
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+    
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
